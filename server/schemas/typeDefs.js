2302967390cc2a1680f82d62675f390a0f122543
aahmad1123 <ayat.asaad@gmail.com>
2024-05-31 17:36:41 -0500

    finished mutations, working on pages

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,7 +10,7 @@ type User{
 type Book{
     authors: [String]
     description: String
-    bookId: String
+    bookId: ID
     image: String
     link: String
     title: String
@@ -21,11 +21,25 @@ type Auth{
     user: User
 }
 
+input SaveBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+}
+
 type Query {
     me: User
 }
 
 type Mutation {
     addUser(username: String, email: String, password: String):Auth
+    login(email: String, password: String):Auth
+    saveBook(bookData: SaveBookInput):User
+    removeBook(bookId: ID):User
 }
 `
+
+module.exports = typeDefs;
